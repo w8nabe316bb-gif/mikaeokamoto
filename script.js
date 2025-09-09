0dc3adc646189f7382839fc178517ee54ffab060
w8nabe316bb <w8nabe.316.bb@gmail.com>
2025-09-09 23:59:53 +0900

    0909更新

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-let numButtonClicks = 0;
-function buttonClicked() {
-    numButtonClicks = numButtonClicks + 1;
-    document.getElementById("mainDiv").textContent =
-        "Button Clicked times: " + numButtonClicks;
-}
+
